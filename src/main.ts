bedf6bb17e032e362676509be0d1c97b4e0aa1d3
Francisco Guilherme <guilhermeee314@gmail.com>
2024-10-08 11:43:00 -0300

    feat: implementando nova origin cors
    implementando nova origin cors

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,10 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://designervanessaximenes.vercel.app',
+    ],
     credentials: true,
     methods: 'GET,POST,PUT,DELETE',
   });
